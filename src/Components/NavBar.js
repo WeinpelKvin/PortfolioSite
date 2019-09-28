6dfb4c5501003d1c74b6f3619c059f8430778d0c
Kevin Weinpel <kevinweinpel@gmail.com>
2019-09-28 16:47:22 -0500

    prework

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,24 +1,26 @@
 import React, {Component} from 'react';
 import '../../public/css/navBar.css';
 
-/*function menuFunction() {
+/*
+function menuFunction() {
     var x = document.getElementById("Navbar");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
         x.className = "navbar";
     }
-}*/
+}
+*/
 
 class NavBar extends Component{
     render(){
         return (
             <div class="navbar" id="Navbar">
-                <a href="index.html">Kevin Weinpel</a>
-                <a href="#home">Portfolio</a>
-                <a href="#news">About</a>
-                <a href="#contact">Resume</a>
-                <a class="icon" onclick="menuFunction()">
+                <a href="index.html" class="navHome">Kevin Weinpel</a>
+                <a href="#home" class="navItem">Portfolio</a>
+                <a href="#news" class="navItem">About</a>
+                <a href="#contact" class="navItem">Resume</a>
+                <a class="icon navItem" onclick="menuFunction()">
                     <svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="phoneMenu"/></svg>
                 </a>
             </div>
