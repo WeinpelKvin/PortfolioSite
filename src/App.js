28d9dece2f3107f703c3fcefda44d97af66eb4ac
Kevin Weinpel <kevinweinpel@gmail.com>
2019-09-30 16:21:48 -0500

    split main componenet out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//import {Router, Route, IndeRoute, hashHistory} from "react-router";
 
 import './App.css';
 import './index.css';
@@ -7,74 +8,7 @@ import '../public/css/style.css'
 import Navbar from './Components/NavBar.js';
 import Header from './Components/Header.js';
 import Footer from './Components/Footer.js';
-import Card from './Components/Card.js';
-
-// Projects
-let Projects = [
-  {
-    name: "Orbit App",
-    image: "/Images/Orbit_Home.jpg",
-    link: "OrbitApp.html",
-    type: "Mobile App"
-  },
-  {
-    name: "Saltwater Fish Tank",
-    image: "Images/FishTank_Home.jpg",
-    link: "SaltWaterFishTank.html",
-    type: "Application"
-  },
-  {
-    name: "Pocket Full Of Sunshine",
-    image: "Images/Sunshine_Home.jpg",
-    link: "PocketFullOfSunshine.html",
-    type: "Movie Branding"
-  },
-  {
-    name: "Synesthesia Zine",
-    image: "Images/Synesthesia_Home.jpg",
-    link: "Synesthesia.html",
-    type: "Zine Booklet"
-  },
-  {
-    name: "Renzo Piano Exhibit",
-    image: "Images/RenzoPiano_Home.jpg",
-    link: "RenzoPiano.html",
-    type: "Exhibit Branding"
-  },
-  {
-    name: "Randsom Note Generator",
-    image: "Images/Generator_Home.jpg",
-    link: "RansomNote.html",
-    type: "Creative Coding"
-  },
-  {
-    name: "Earth 2.0 Movie",
-    image: "Images/Earth_Home.jpg",
-    link: "Earth.html",
-    type: "Movie Branding"
-  },
-  {
-    name: "Udelco",
-    image: "Images/Udelco_Home.jpg",
-    link: "Udelco.html",
-    type: "Commercial Website",
-    description: "A desktop and mobile website design for a thrift store in Hawthorne New Jersey that was found in the '70s. They specialize in vintage clothes from the '60s, '70s, and '80s."
-  },
-  {
-    name: "Motherboard",
-    image: "Images/Motherboard_Home.jpg",
-    link: "Motherboard.html",
-    type: "Magazine"
-  }
-]
-
-const pieces = [];
-
-for (let i = 0; i < Projects.length; i++) {
-  pieces.push(<Card name={Projects[i].name} image={Projects[i].image} link={Projects[i].link} type={Projects[i].type}/>)
-}
-
-console.log(pieces);
+import MainComponent from './Components/MainComponent.js';
 
 class App extends Component {
   render() {
@@ -82,7 +16,7 @@ class App extends Component {
       <div class="wrapper">
         <Navbar/>
         <Header/>
-        <div class="main wrapper">{pieces}</div>
+        <MainComponent/>
         <Footer/>
       </div>
     );
