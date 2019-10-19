cf037a637e2cb4d6ff57fd74a087785884d8a2a3
Kevin Weinpel <kevinweinpel@gmail.com>
2019-10-19 18:16:50 -0500

    added the collapse menu feature when an item is clicked

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -20,12 +20,18 @@ class NavBar extends Component{
                 x.className = "navbar";
             }
         }
+        function collapse(){
+            var x = document.getElementById("Navbar");
+            if (x.className == "navbar responsive"){
+                x.className = "navbar";
+            }
+        }
         return (
             <div className="navbar" id="Navbar">
                 <Link to="/" className="navHome">Kevin Weinpel</Link>
-                <Link to="/portfolio" className="navItem">Portfolio</Link>
-                <Link to="/about" className="navItem">About</Link>
-                <Link to="/resume" className="navItem">Resume</Link>
+                <Link to="/portfolio" className="navItem" onClick={collapse}>Portfolio</Link>
+                <Link to="/about" className="navItem" onClick={collapse}>About</Link>
+                <Link to="/resume" className="navItem" onClick={collapse}>Resume</Link>
                 <a className="icon navItem" id="menuBtn" onClick={menuPopup}>
                     <svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" className="phoneMenu"/></svg>
                 </a>
