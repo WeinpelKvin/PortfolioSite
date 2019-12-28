6aae4369f43a1f1c88c1d7d6939033c213a3c477
Kevin Weinpel <kevinweinpel@gmail.com>
2019-12-27 21:26:57 -0500

    style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,11 +13,13 @@ class Header extends Component {
                     <img src="https://www.factsonhand.com/images/1/hand-nodules@3x_1113x1020.png" style={ImageStyle} alt=""></img>
                 </div>
                 <div className="headerText column">
-                    <h1>I'm a UX / UI Designer in Dallas, Texas interested in design, programming, and what falls in between.</h1>
+                    <h1>Kevin Weinpel</h1>
+                    <h2>UX / UI Designer in Dallas, Texas creating design, programming, and what falls in between.</h2>
                     <Link to="/portfolio" className="button buttonHeader">See My Work</Link>
                 </div>
                 <div className="headerSubtext columnHalf">
                     <p>Scroll down to see my design portfolio or view some of my other informatiom through my social media.</p>
+                    <p>Thanks for stopping by</p>
                 </div>
             </header>
         )
