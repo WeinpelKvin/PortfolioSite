3a712ffd51ac8d9cd66e42a0a991ee3bb2e36ff2
Kevin <kevinweinpel@gmail.com>
2020-03-12 19:05:12 -0500

    modal popup

--- src/Components/ContentComponent.js
+++ src/Components/ContentComponent.js
@@ -15,10 +15,8 @@ class ContentComponent extends Component{
 
         // Determines the current, next, or previous project in array
         const project = Projects[this.props.location.params.key];
-
         let nextProject;
         let previousProject;
-        console.log("Key " + project.key);
 
         if((project.key+1) <= Projects.length-1) {
             nextProject= (project.key+1); 
@@ -26,7 +24,6 @@ class ContentComponent extends Component{
         else {
             nextProject = 0; 
         }
-        console.log("Next " + nextProject);
 
         if((project.key-1) >= 0) {
             previousProject = (project.key-1); 
@@ -34,42 +31,35 @@ class ContentComponent extends Component{
         else {
             previousProject = (Projects.length-1);
         }
-        console.log("Previous " + previousProject);
 
+        function ModalPopUp(key) {
+            document.getElementById("imageModal").style.display = "block";
+        }
 
-        function popUpModal(){
-            alert("image clicked");
+        function CloseModal() {
+            document.getElementById("imageModal").style.display = "none";
         }
 
         const RenderContent=()=>{
             const contentList = [];
-            const Image = (props) =>{
-                return(
-                    <img 
-                        src={props.name} 
-                        className="contentImages" 
-                        alt=""
-                        onClick={popUpModal}
-                    />
-                )
+            const Image=(props)=>{
+                return(<img src={props.name} className="contentImages" alt={props.name} onClick={()=>{ModalPopUp(props.imageKey)}} />)
             }
             const Pg = (props) =>{
-                return(
-                    <p>{props.name}</p>
-                )
+                return(<p>{props.name}</p>)
             }
             const H1 = (props) =>{
-                return(
-                    <h1>(props.name)</h1>
-                )
+                return(<h1>(props.name)</h1>)
             }
 
             // Figures out what type of content is going to be rendered
+            let imageKey = 0;
             for (let i = 0; i < project.content.length; i++) {
                 const len = project.content[i].length;
                 let sub = project.content[i].substring(len-4, len);
                 if(sub === ".png" || sub === ".jpg"){
-                    contentList.push(<Image id="myImg"key={i} name={project.content[i]}/>)
+                    contentList.push(<Image id="myImg" key={i} imageKey={imageKey} name={project.content[i]}/>)
+                    imageKey++;
                 }else if(sub === " h1"){
                     contentList.push(<H1 key={i} name={project.content[i]}/>)
                 }else{
@@ -87,39 +77,46 @@ class ContentComponent extends Component{
         }
 
         return (    
-            <div className="Contentwrapper">
-                <RenderContent/>
-                <article className="aside aside-1">
-                    <div className="infoPanel">
-                        <Link to={{
-                            pathname: "/contentPage",
-                            params: {   
-                                key: previousProject,
-                                name: Projects[previousProject].name,
-                                image: Projects[previousProject].image,
-                                type: Projects[previousProject].type,
-                                shortDescription: Projects[previousProject].shortDescription,
-                                longDescription: Projects[previousProject].longDescription,
-                                content: Projects[previousProject].content
-                            }
-                        }} className="button infoButton">prev</Link>      
-                        <Link to={{
-                            pathname: "/contentPage",
-                            params: {   
-                                key: nextProject,
-                                name: Projects[nextProject].name,
-                                image: Projects[nextProject].image,
-                                type: Projects[nextProject].type,
-                                shortDescription: Projects[nextProject].shortDescription,
-                                longDescription: Projects[nextProject].longDescription,
-                                content: Projects[nextProject].content
-                            }
-                        }} className="button infoButton">next</Link> 
-                        <p className="infoDescription">{project.longDescription}</p>  
-                    </div>
-                </article>
-                <img className="arrow" src={arrow} alt="" onClick={()=>{window.scrollTo({top: 0, left:0, behavior: 'smooth'})}}></img>
+        <div className="Contentwrapper">
+            <div id="imageModal" className="modal">
+                <div className="modal-content">
+                    <button onClick={()=>{CloseModal()}}>Close</button>
+                    <img className="modalImage" src="Images/Orbit/OrbitContent_1.png" alt="Orbit" />
+                </div>
             </div>
+            <RenderContent/>
+            <article className="aside aside-1">
+                <div className="infoPanel">
+                    <Link to={{
+                        pathname: "/contentPage",
+                        params: {   
+                            key: previousProject,
+                            name: Projects[previousProject].name,
+                            image: Projects[previousProject].image,
+                            type: Projects[previousProject].type,
+                            shortDescription: Projects[previousProject].shortDescription,
+                            longDescription: Projects[previousProject].longDescription,
+                            content: Projects[previousProject].content
+                        }
+                    }} className="button infoButton">prev</Link>      
+                    <Link to={{
+                        pathname: "/contentPage",
+                        params: {   
+                            key: nextProject,
+                            name: Projects[nextProject].name,
+                            image: Projects[nextProject].image,
+                            type: Projects[nextProject].type,
+                            shortDescription: Projects[nextProject].shortDescription,
+                            longDescription: Projects[nextProject].longDescription,
+                            content: Projects[nextProject].content
+                        }
+                    }} className="button infoButton">next</Link> 
+                    <p className="infoDescription">{project.longDescription}</p>  
+                </div>
+                <p>hi</p>
+            </article>
+            <img className="arrow" src={arrow} alt="" onClick={()=>{window.scrollTo({top: 0, left:0, behavior: 'smooth'})}}></img>
+        </div>
         )
     }
 }
