9fb23bd2d16056ac531c71b52572d0f7b1daa89c
Kevin Weinpel <kevinweinpel@gmail.com>
2020-04-02 12:15:01 -0500

    got modal to work

--- src/Components/ContentComponent.js
+++ src/Components/ContentComponent.js
@@ -3,22 +3,24 @@ import '../../public/css/styleContent.css';
 import '../../public/css/style.css'; 
 import '../../public/css/Card.css';
 import { Link } from "react-router-dom";
-
 import arrow from "../../public/Images/Icons/Arrow.svg"
 import Projects from '../Projects.json';
-import Image from '../Components/ProjectContent/Image'
 import H1 from '../Components/ProjectContent/H1';
 import Pg from '../Components/ProjectContent/Pg';
 
 class ContentComponent extends Component{
+
     constructor(props) {
         super(props);
-        this.state = { image: "Images/Orbit/OrbitContent_2.png" };
-        
+        this.state = { image: "Images/Orbit/OrbitContent_2.png" };   
     }
 
-    render(){  
+    ModalPopUp(name) {
+        this.setState({image: name})
+        document.getElementById("imageModal").style.display = "block";            
+    }
 
+    render() {  
         // Determines the current, next, or previous project in array
         const project = Projects[this.props.location.params.key];        
         let nextProject;
@@ -27,11 +29,11 @@ class ContentComponent extends Component{
         if((project.key+1) <= Projects.length-1) {
             nextProject= (project.key+1); 
         }
-        else {
+        else{
             nextProject = 0; 
         }
 
-        if((project.key-1) >= 0) {
+        if ((project.key-1) >= 0) {
             previousProject = (project.key-1); 
         }
         else {
@@ -41,51 +43,50 @@ class ContentComponent extends Component{
         function CloseModal() {
             document.getElementById("imageModal").style.display = "none";
         }
-        
-        // function ModalPopUp(name, key) {
-        //     // console.log(this)
-        //     // this.setState({image: name})
-        //     // console.log(name)
-        //     console.log("hi")
-        //     // document.getElementById("imageModal").style.display = "block";
-            
-        // }
-        
-        const RenderContent = () => {
-            const contentList = [];
 
-            // ModalPopUp = (name, key) => {
-            //     // console.log(this)
-            //     // this.setState({image: name})
-            //     // console.log(name)
-            //     console.log("hi")
-            //     // document.getElementById("imageModal").style.display = "block";
-                
-            // }
+        const contentList = [];
+        //let imageKey = 0;
 
-            // Figures out what type of content is going to be rendered
-            let imageKey = 0;
-            for (let i = 0; i < project.content.length; i++) {
-                const len = project.content[i].length;
-                let sub = project.content[i].substring(len-4, len);
-                if(sub === ".png" || sub === ".jpg"){
-                    contentList.push(<Image id="myImg" key={i} imageKey={imageKey} name={project.content[i]} />)
-                    imageKey++;
-                }else if(sub === " h1"){
-                    contentList.push(<H1 key={i} name={project.content[i]}/>)
-                }else{
-                    contentList.push(<Pg key={i} name={project.content[i]}/>)
-                }
+        // Figures out what type of content is going to be rendered
+        for (let i = 0; i < project.content.length; i++) {
+            const len = project.content[i].length;
+            let sub = project.content[i].substring(len-4, len);
+
+            if(sub === ".png" || sub === ".jpg"){
+                contentList.push(<img src={project.content[i]} key={i} className="contentImages" alt={project.content[i]} onClick={()=>{this.ModalPopUp(project.content[i])}}/>)
+            }else if(sub === " h1"){
+                contentList.push(<H1 key={i} name={project.content[i]}/>)
+            }else{
+                contentList.push(<Pg key={i} name={project.content[i]}/>)
             }
-            return(
-                <div className="main">
-                    <div className="projectHeader">
-                        <h1>{project.name}</h1>
-                    </div>
-                    {contentList}
-                </div>
-            )
         }
+
+        // const RenderContent = () => {
+        //     const contentList = [];
+
+        //     // Figures out what type of content is going to be rendered
+        //     let imageKey = 0;
+        //     for (let i = 0; i < project.content.length; i++) {
+        //         const len = project.content[i].length;
+        //         let sub = project.content[i].substring(len-4, len);
+        //         if(sub === ".png" || sub === ".jpg"){
+        //             contentList.push(<Image id="myImg" key={i} imageKey={imageKey} name={project.content[i]} />)
+        //             imageKey++;
+        //         }else if(sub === " h1"){
+        //             contentList.push(<H1 key={i} name={project.content[i]}/>)
+        //         }else{
+        //             contentList.push(<Pg key={i} name={project.content[i]}/>)
+        //         }
+        //     }
+        //     return(
+        //         <div className="main">
+        //             <div className="projectHeader">
+        //                 <h1>{project.name}</h1>
+        //             </div>
+        //             {contentList}
+        //         </div>
+        //     )
+        // }
 
         return (    
             <div className="Contentwrapper">
@@ -95,7 +96,12 @@ class ContentComponent extends Component{
                         <img className="modalImage" src={this.state.image} alt="Orbit" />
                     </div>
                 </div>   
-                <RenderContent/>
+                <div className="main">
+                    <div className="projectHeader">
+                        <h1>{project.name}</h1>
+                    </div>
+                    {contentList}
+                </div>
                 <article className="aside aside-1">
                     <div className="infoPanel">
                         <Link to={{
